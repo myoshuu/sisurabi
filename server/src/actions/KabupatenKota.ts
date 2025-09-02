d8552b1f779a3cab52a0b1be3ae94f35a7af2395
myoemoo <myoshu.me@gmail.com>
2025-09-02 22:32:48 +0700

    refactor: limiting file created

--- server/src/actions/KabupatenKota.ts
+++ server/src/actions/KabupatenKota.ts
@@ -12,3 +12,20 @@ export const indexKabupatenKota = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Terjadi kesalahan sistem." });
   }
 };
+
+export const getPasarByKabupaten = async (req: Request, res: Response) => {
+  try {
+    const { kabupatenKotaId } = req.params;
+    const pasar = await prisma.pasar.findMany({
+      where: { kabupatenKotaId },
+      orderBy: { nama: "asc" },
+    });
+    return res.status(400).json({
+      message: "Mengambil semua data pasar berdasarkan kabupaten",
+      pasar,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Terjadi kesalahan sistem." });
+  }
+};
---
+++ server/src/routes/KabupatenKotaRoute.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { isAuthenticated } from "@/middleware/auth";
+import { authorize } from "@/middleware/authorize";
+import {
+  getPasarByKabupaten,
+  indexKabupatenKota,
+} from "@/actions/KabupatenKota";
+
+const router = Router();
+
+router.get(
+  "/",
+  isAuthenticated,
+  authorize("SUPER ADMIN", "ADMIN", "USER"),
+  indexKabupatenKota
+);
+
+router.get(
+  "/:id/pasar",
+  isAuthenticated,
+  authorize("SUPER ADMIN", "ADMIN", "USER"),
+  getPasarByKabupaten
+);
+
+export default router;
