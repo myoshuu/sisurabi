ee247e849187c44e1c088705f9298e3a8315e7f5
myoshuu <myoshu.me@gmail.com>
2025-09-17 00:29:57 +0700

    test: push with myoshuu name

--- src/pages/dashboard/Absensi.tsx
+++ src/pages/dashboard/Absensi.tsx
@@ -59,7 +59,7 @@ const Absensi: React.FC = () => {
   const [absensi, setAbsensi] = useState<AbsensiItem[]>([]);
   const [deletingId, setDeletingId] = useState<string | null>(null);
   const [previewSrc, setPreviewSrc] = useState<string | null>(null);
-
+  // Testing for push
   const navigate = useNavigate();
 
   const absoluteUrl = (path: string | null | undefined) => {
